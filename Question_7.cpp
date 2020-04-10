5ed7036e11deb2f36cfb09ae68e4f0c966e54121
psahithireddy <62285383+psahithireddy@users.noreply.github.com>
2020-04-10 14:14:24 +0530

    Rename que7.cpp to Question_7.cpp

--- Question_7.cpp
+++ Question_7.cpp
@@ -4,15 +4,15 @@
 	Create an array dist[] of size |V| with all values as infinite except dist[src] where src is source vertex.
 
 2) This step calculates shortest distances. Do following |V|-1 times where |V| is the number of vertices in given graph.
-…..a) Do following for each edge u-v
-………………If dist[v] > dist[u] + weight of edge uv, then update dist[v]
-………………….dist[v] = dist[u] + weight of edge uv
+â€¦..a) Do following for each edge u-v
+â€¦â€¦â€¦â€¦â€¦â€¦If dist[v] > dist[u] + weight of edge uv, then update dist[v]
+â€¦â€¦â€¦â€¦â€¦â€¦â€¦.dist[v] = dist[u] + weight of edge uv
 
 3) This step reports if there is a negative weight cycle in graph.
 	 Do following for each edge u-v
-		……If dist[v] > dist[u] + weight of edge uv, 
-			then “Graph contains negative weight cycle”
-The idea of step 3 is, step 2 guarantees shortest distances if graph doesn’t contain negative weight cycle. 
+		â€¦â€¦If dist[v] > dist[u] + weight of edge uv, 
+			then â€œGraph contains negative weight cycleâ€
+The idea of step 3 is, step 2 guarantees shortest distances if graph doesnâ€™t contain negative weight cycle. 
 If we iterate through all edges one more time and get a shorter path for any vertex, then there is a negative weight cycle
 */
 #include<iostream>
